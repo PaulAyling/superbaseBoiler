bdb8efc26ede3d782557ba36e98c99394772716e
PaulAyling <jimmyyukka@gmail.com>
2022-02-13 19:14:59 +0100

    setting up

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,9 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
+import { config } from 'dotenv';
+  import replace from '@rollup/plugin-replace';
+  import json from '@rollup/plugin-json'
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -74,3 +77,22 @@ export default {
 		clearScreen: false
 	}
 };
+
+
+
+  export default {
+    plugins: [
+    replace({
+            __api: JSON.stringify({
+                env: {
+                    isProd: production,
+                    ...config().parsed // attached the .env config
+                }
+            }),
+            delimiters: ['', '']
+        }),
+        json(),
+    // ...
+    ],
+    // ...
+  }
